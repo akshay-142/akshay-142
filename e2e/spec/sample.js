560f84e7b51851a27490dce37127886afdfe1344
qa-akshay-pb <100873144+akshay-142@users.noreply.github.com>
2024-02-09 00:59:39 +0530

    fix: basic

---
+++ e2e/spec/sample.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress"/>
+
+function verify() {
+  for (i = 0; i < 15; i++) {
+    return yes;
+  }
+}
+
+verify();
+
+describe("", () => {
+  it("", () => {
+    cy.visit("https://www.google.com");
+  });
+});
